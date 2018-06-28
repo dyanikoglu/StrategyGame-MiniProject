4bf61ae55be70d300e83081e19c9b264a993369d
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2018-06-28 17:20:48 +0300

    Fast prototyping for placing buildings on map

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,46 +5,47 @@ using UnityEngine.EventSystems;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 ResetCamera;
-    private Vector3 Origin;
-    private Vector3 Diference;
-    private bool Drag = false;
-    public BoxCollider2D tilemapBounds;
-    public bool canBeDragged;
+    private Vector3 _origin;
+    private Vector3 _difference;
+    private bool _drag = false;
+    private bool _canBeDragged;
 
-    void Start()
+    public BoxCollider2D TilemapBounds;
+ 
+    private void Start()
     {
-        canBeDragged = false;
-        ResetCamera = Camera.main.transform.position;
+        _canBeDragged = false;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
-        if (Input.GetMouseButton(0) && canBeDragged)
+        // If left mouse is pressed and camera is available to drag, start dragging
+        if (Input.GetMouseButton(0) && _canBeDragged)
         {
-            Diference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
-            if (Drag == false)
+            _difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            if (_drag == false)
             {
-                Drag = true;
-                Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                _drag = true;
+                _origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
         }
+        // Camera is not available to drag
         else if(!Input.GetMouseButton(0))
         {
-            Drag = false;
+            _drag = false;
         }
 
-        if (Drag)
+        // Update drag position
+        if (_drag)
         {
-            Camera.main.transform.position = Origin - Diference;
+            Camera.main.transform.position = _origin - _difference;
         }
 
+        // Clamp camera movement to map bounds
         float vertExtent = this.GetComponent<Camera>().orthographicSize;
         float horizExtent = vertExtent * Screen.width / Screen.height;
-
         Vector3 linkedCameraPos = this.GetComponent<Camera>().transform.position;
-        Bounds areaBounds = tilemapBounds.bounds;
-
+        Bounds areaBounds = TilemapBounds.bounds;
         this.GetComponent<Camera>().transform.position = new Vector3(
             Mathf.Clamp(linkedCameraPos.x, areaBounds.min.x + horizExtent, areaBounds.max.x - horizExtent),
             Mathf.Clamp(linkedCameraPos.y, areaBounds.min.y + vertExtent, areaBounds.max.y - vertExtent),
@@ -53,6 +54,6 @@ public class CameraMovement : MonoBehaviour
 
     public void SetCanBeDragged(bool canBeDragged)
     {
-        this.canBeDragged = canBeDragged;
+        this._canBeDragged = canBeDragged;
     }
 }
--- Assets/ConstructionPanel.cs
+++ Assets/ConstructionPanel.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConstructionPanel : MonoBehaviour {
+public class ConstructionPanel : MonoBehaviour
+{
+
+    public GameObject BarracksMenuObject;
+    public GameObject PowerPlantMenuObject;
+    public GameObject ConstructionObjectsContent;
+
+    public float XOffset;
+    public float YOffset;
+    public float XSpacing;
+    public float YSpacing;
+
+    public int RowCount;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    FillConstructionPanel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void FillConstructionPanel()
+    {
+        for (int i = 0; i < RowCount; i++)
+        {
+            GameObject obj = Instantiate(BarracksMenuObject, ConstructionObjectsContent.transform);
+            obj.transform.localPosition = new Vector3(obj.transform.localPosition.x + XOffset, obj.transform.localPosition.y - YOffset - YSpacing * i, obj.transform.localPosition.z);
+
+            obj = Instantiate(PowerPlantMenuObject, ConstructionObjectsContent.transform);
+            obj.transform.localPosition = new Vector3(obj.transform.localPosition.x + XOffset + XSpacing, obj.transform.localPosition.y - YOffset - YSpacing * i, obj.transform.localPosition.z);
+        }
+    }
 }
